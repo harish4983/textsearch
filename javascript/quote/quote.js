48b5c0778320fa3848f958a3c07814ef31aa8b9f
harish4983 <abnhharish@gmail.com>
2022-11-09 15:01:47 +0530

    SOME CHANGES

--- javascript/quote/quote.js
+++ javascript/quote/quote.js
@@ -2,7 +2,7 @@ function gen(){
     fetch("https://api.quotable.io/random")
     .then( x => x.json())
     .then(y =>{
-        document.getElementById("quote").innerHTML=y.content;
-        document.getElementById("author").innerHTML="~"+y.author;
+         document.getElementById("quote").innerHTML=y.content;
+         document.getElementById("author").innerHTML="~"+y.author;
     })
 }
