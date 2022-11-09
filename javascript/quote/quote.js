35152908a1e34979718cf3e861f7d95b887675f7
harish4983 <abnhharish@gmail.com>
2022-11-09 14:21:10 +0530

    mm

--- javascript/quote/quote.js
+++ javascript/quote/quote.js
@@ -1,7 +1,8 @@
-
+function gen(){
     fetch("https://api.quotable.io/random")
     .then( x => x.json())
     .then(y =>{
         document.getElementById("quote").innerHTML=y.content;
         document.getElementById("author").innerHTML="~"+y.author;
     })
+}
